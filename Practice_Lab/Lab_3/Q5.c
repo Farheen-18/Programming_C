d483f8d1386fc02b4e67a2b1cb8dab6a814f083d
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-01 23:00:15 +0530

    Update Q5.c

--- Practice_Lab/Lab_3/Q5.c
+++ Practice_Lab/Lab_3/Q5.c
@@ -8,7 +8,8 @@ Consider the pattern shown below, which gets printed based on the input value of
   4 5 6 7 6 5 4     
 5 6 7 8 9 8 7 6 5   
 */
-
+/*
+ //       By me: 
 int main()
 {
   int N;
@@ -28,4 +29,43 @@ int main()
     printf("\n");
     }
 }
+*/
+//By AI
+#include <stdio.h>
+
+int main() {
+    int N;
+    
+    // Input the value of N
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+    
+    // Loop through each row
+    for (int i = 1; i <= N; i++) {
+        // Print leading spaces
+        for (int j = 1; j <= N - i; j++) {
+            printf("  ");
+        }
+
+        // Print increasing numbers
+        int num = i;
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", num);
+            num++;
+        }
+
+        // Print decreasing numbers
+        num -= 2;
+        for (int j = 1; j < i; j++) {
+            printf("%d ", num);
+            num--;
+        }
+
+        // Move to the next line
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
