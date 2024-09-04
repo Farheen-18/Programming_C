fd91b5341f72c211bd90efd6232def3210332bb9
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-05 00:51:39 +0530

    Create Q2.c

---
+++ Practice_Lab/Lab_5/Q2.c
@@ -0,0 +1,16 @@
+/* The program checks if the given number is a perfect number:
+
+In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding
+the number itself. For instance, 6 has divisors 1, 2, and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
+Testcase 1
+Input a number to check: 371
+371 is NOT a Perfect number.
+
+Testcase 2 
+Input a number to check: 496
+496 is a Perfect number.
+
+Testcase 3
+Input a number to check: 40
+40 is NOT a Perfect number. */
