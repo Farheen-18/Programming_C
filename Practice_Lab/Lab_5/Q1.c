e73c3d6d23750e21c7187f99f1bffca37f63967f
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-05 01:17:58 +0530

    Update Q1.c

--- Practice_Lab/Lab_5/Q1.c
+++ Practice_Lab/Lab_5/Q1.c
@@ -1,40 +1,56 @@
 /* PracticeLab2_Question1
 
-The program checks if the given number is:
-
-1. Armstrong number:
+The program checks if the given number is an Armstrong number:
 Armstrong number is a number that is equal to the sum of cubes of its digits. For example, 0, 1, 153, 370, and 371 are
 the Armstrong numbers.371 = 3^3+7^3+1^3 (^ means the power of).
-
-2. Perfect number:
-In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding
-the number itself. For instance, 6 has divisors 1, 2, and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number.
 
 Testcase 1
 Input a number to check: 371
 371 is an Armstrong number.
-371 is NOT a Perfect number.
 
 Testcase 2 
 Input a number to check: 496
 496 is NOT an Armstrong number.
-496 is a Perfect number.
 
 Testcase 3
 Input a number to check: 40
 40 is NOT an Armstrong number.
-40 is NOT a Perfect number.
 */
 //Part 1 -- Armstrong number
 
-#inlcude <stdio.h>
+#include <stdio.h>
 
 int is_armstrong(int num);
-int is_perfect_
 
+int main()
+{
+  int num;
+  scanf("%d", &num);
 
+  if (is_armstrong(num)==1)
+  {
+    printf("%d is an armstrong number.\n", num);
+  }
+  else printf("%d is not an armstrong number.\n", num);
 
+  return 0;
+}
 
+int is_armstrong(int num)
+{
+  //323 %10 = 3, num=num/10 = 32
+  int store_num = num;
+  int check_armstrong = 0; //to calculate the cubes
+  int remainder=0;
+  if (num==1 || num==0) return 1;
 
+  while (num != 0 )
+    {
+      remainder = num%10;
+      check_armstrong +=  (remainder)*(remainder)*(remainder);
+      num = num/10;
+    }
+  if (check_armstrong == store_num) return 1;
 
-
+  return 0;
+}
