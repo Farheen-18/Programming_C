d98d12753f8b264202cde6097e13a36b09a125dc
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-05 00:50:13 +0530

    Create Q1.c

---
+++ Practice_Lab/Lab_5/Q1.c
@@ -0,0 +1,40 @@
+/* PracticeLab2_Question1
+
+The program checks if the given number is:
+
+1. Armstrong number:
+Armstrong number is a number that is equal to the sum of cubes of its digits. For example, 0, 1, 153, 370, and 371 are
+the Armstrong numbers.371 = 3^3+7^3+1^3 (^ means the power of).
+
+2. Perfect number:
+In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding
+the number itself. For instance, 6 has divisors 1, 2, and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
+Testcase 1
+Input a number to check: 371
+371 is an Armstrong number.
+371 is NOT a Perfect number.
+
+Testcase 2 
+Input a number to check: 496
+496 is NOT an Armstrong number.
+496 is a Perfect number.
+
+Testcase 3
+Input a number to check: 40
+40 is NOT an Armstrong number.
+40 is NOT a Perfect number.
+*/
+//Part 1 -- Armstrong number
+
+#inlcude <stdio.h>
+
+int is_armstrong(int num);
+int is_perfect_
+
+
+
+
+
+
+
