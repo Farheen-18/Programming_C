f8e7be531fcc5f3914d21ac8aed32e9bea6bbb53
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-05 01:18:55 +0530

    Create Q3.c

---
+++ Practice_Lab/Lab_5/Q3.c
@@ -0,0 +1,22 @@
+/* Complete the code to find if the given number is a palindrome or not.
+A number is a palindrome when its digits in reverse order form the same number. The reverse digits of 12321 are 12321,
+which equals the same number. Hence, 12321 is a palindrome. The reverse digits of 12301 are 10321, which are not equal
+to the same number. Hence, 12301 is not a palindrome.
+
+
+Testcase 1
+Please Enter a number: 12321
+12321 is a palindrome.
+
+Testcase 2
+Please Enter a number: 12
+12 is NOT a palindrome.
+
+Testcase 3
+Please Enter a number: 121
+121 is a palindrome.
+
+Testcase 4
+Please Enter a number: 11
+11 is a palindrome. */
+
