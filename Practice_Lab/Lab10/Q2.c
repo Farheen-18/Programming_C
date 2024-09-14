5721903f18ce65f9402d0cb073d70470dd680f90
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-14 16:37:39 +0530

    Create Q2.c

---
+++ Practice_Lab/Lab10/Q2.c
@@ -0,0 +1,9 @@
+PracticeLab3_Question2
+
+Consider a program that creates a string (a character array that ends with a \0) by taking it as input from the user. 
+  You are required to write a program that finds the average of the ASCII values of all the characters in the given character array (excluding the final \0). 
+  Then your program should display the character equivalent of that average ASCII value computed.
+
+For example, if our character array is char arr[6] = {'a','b','c','d','e','\0'}, 
+your program should display c, 
+which is the character equivalent of the average of the ASCII values of the characters entered by the user
