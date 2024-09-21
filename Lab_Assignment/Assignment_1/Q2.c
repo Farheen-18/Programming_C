c24470a55f1590d5e116589403f5417e703fff6e
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-21 12:18:33 +0530

    Create Q2.c

---
+++ Lab_Assignment/Assignment_1/Q2.c
@@ -0,0 +1,33 @@
+Grades_Question
+
+Write a C program that prepares a report card for a student. The function report()  takes value marks from the user (integer values ranging from 0 to 100). Based on the marks, the function should assign and print the corresponding grade according to the following grading key:
+
+90 - 100: Grade A
+
+80 - 89: Grade B
+
+70 - 79: Grade C
+
+60 - 69: Grade D
+
+50 - 59: Grade E
+
+0 - 49: Grade F
+
+Your function should also be capable of handling invalid inputs, such as marks outside the range 0-100. The function should not print anything.
+
+The function returns a character that is assigned to the student (their grade). In case of invalid input, return 'X'. 
+
+
+Enter your marks:
+
+85
+
+Grade: B
+
+Enter your marks:
+
+103
+
+Invalid marks entered. Please enter a value between 0 and 100.
+
