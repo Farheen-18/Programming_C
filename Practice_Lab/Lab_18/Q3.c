f798e766f19e73fc64f7308f9397597276452b89
Farheen <143966590+Farheen-18@users.noreply.github.com>
2024-09-25 21:07:40 +0530

    Create Q3.c

---
+++ Practice_Lab/Lab_18/Q3.c
@@ -0,0 +1,21 @@
+PracticeLab1_Question3
+Write a program that takes two distances as input. The distances are in the form of X kilometers and Y meters.
+
+For example, 23 68 means 23 kilometers and 68 meters. Store the distances in a structure. After that, find the
+difference between the two distances. Store this difference in another structure. (1000 meter = 1 kilometer). It is
+guaranteed that the second distance is greater than the first distance.
+
+A structure definition has been provided, complete the missing parts of the code!
+
+Testcase1
+
+    Input:                                             Input:
+
+         Enter first distance = 5 40                    Enter first distance = 1 500
+         Enter second distance = 10 60                  Enter second distance = 2 0
+
+    Output:                                           Output:
+
+        The difference is = 5 kilometers and 20 meters     The difference is = 0 kilometers and 500 meters
+
+    
